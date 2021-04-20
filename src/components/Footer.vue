e6878f44ef31cf08fd05d2feb1bcf4700c9e69fc
BSCTrade Developer <support@bsctrades.app>
2021-04-20 12:25:07 +0530

    Added GIthub Link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,7 +12,7 @@
               <li><a target="_blank" href="http://www.twitter.com/BSCtrades"><i class="fab fa-twitter"></i></a></li>
               <li><a target="_blank" href="https://t.me/bsctrades"><i class="fab fa-telegram-plane"></i></a></li>
               <li><a target="_blank" href="https://medium.com/@bsctradenow"><i class="fab fa-medium-m"></i></a></li>
-              <li><a target="_blank" href="#"><i class="fab fa-github"></i></a></li>
+              <li><a target="_blank" href="https://github.com/bsctrades/web"><i class="fab fa-github"></i></a></li>
               <!-- <li><span><a href="resources/Whitepaper.pdf" download="" target="blank"><span> whitepaper </span></a></span></li>    -->
             </ul>
             
